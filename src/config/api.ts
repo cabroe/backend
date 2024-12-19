5ac3bdc67cda8c2026952fe4ae1d1a74682ed5e9
cabroe <cabroe@users.noreply.github.com>
2024-12-19 10:37:27 +0100

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api/v1'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api-test.kasit.de/api/v1'
 
 export const API_ROUTES = {
   // Auth
